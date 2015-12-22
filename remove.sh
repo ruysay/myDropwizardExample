b4a8c52b74e545c36d2da922b1f2e0e418c79a46
fl1-lawrencechiang <lawrence.chiang@quantatw.com>
2015-12-22 16:23:25 +0800

    remove unused objects

---
+++ remove.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+	git rm target/apidocs/allclasses-frame.html
+	git rm target/apidocs/allclasses-noframe.html
+	git rm target/apidocs/com/lawrence/web/HelloWorldApplication.html
+	git rm target/apidocs/com/lawrence/web/HelloWorldConfiguration.html
+	git rm target/apidocs/com/lawrence/web/HelloWorldResource.html
+	git rm target/apidocs/com/lawrence/web/Saying.html
+	git rm target/apidocs/com/lawrence/web/UserResource.html
+	git rm target/apidocs/com/lawrence/web/class-use/HelloWorldApplication.html
+	git rm target/apidocs/com/lawrence/web/class-use/HelloWorldConfiguration.html
+	git rm target/apidocs/com/lawrence/web/class-use/HelloWorldResource.html
+	git rm target/apidocs/com/lawrence/web/class-use/Saying.html
+	git rm target/apidocs/com/lawrence/web/class-use/UserResource.html
+	git rm target/apidocs/com/lawrence/web/db/DAOResponse.html
+	git rm target/apidocs/com/lawrence/web/db/UserDAO.html
+	git rm target/apidocs/com/lawrence/web/db/class-use/DAOResponse.html
+	git rm target/apidocs/com/lawrence/web/db/class-use/UserDAO.html
+	git rm target/apidocs/com/lawrence/web/db/package-frame.html
+	git rm target/apidocs/com/lawrence/web/db/package-summary.html
+	git rm target/apidocs/com/lawrence/web/db/package-tree.html
+	git rm target/apidocs/com/lawrence/web/db/package-use.html
+	git rm target/apidocs/com/lawrence/web/health/MyHealthCheck.html
+	git rm target/apidocs/com/lawrence/web/health/class-use/MyHealthCheck.html
+	git rm target/apidocs/com/lawrence/web/health/package-frame.html
+	git rm target/apidocs/com/lawrence/web/health/package-summary.html
+	git rm target/apidocs/com/lawrence/web/health/package-tree.html
+	git rm target/apidocs/com/lawrence/web/health/package-use.html
+	git rm target/apidocs/com/lawrence/web/package-frame.html
+	git rm target/apidocs/com/lawrence/web/package-summary.html
+	git rm target/apidocs/com/lawrence/web/package-tree.html
+	git rm target/apidocs/com/lawrence/web/package-use.html
+	git rm target/apidocs/constant-values.html
+	git rm target/apidocs/deprecated-list.html
+	git rm target/apidocs/help-doc.html
+	git rm target/apidocs/index-all.html
+	git rm target/apidocs/index.html
+	git rm target/apidocs/overview-frame.html
+	git rm target/apidocs/overview-summary.html
+	git rm target/apidocs/overview-tree.html
+	git rm target/apidocs/package-list
+	git rm target/apidocs/script.js
+	git rm target/apidocs/stylesheet.css
+	git rm target/findbugs-exclude.xml
+	git rm target/findbugs.xml
+	git rm target/findbugsXml.xml
+	git rm target/hello-1.0-SNAPSHOT-javadoc.jar
+	git rm target/hello-1.0-SNAPSHOT-sources.jar
+	git rm target/javadoc-bundle-options/javadoc-options-javadoc-resources.xml
+	git rm target/javadoc-bundle-options/package-list
+
+
+
+
+
